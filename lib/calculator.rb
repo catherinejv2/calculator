996c9cbe3119a2cfa3d8e248f69e7b6cbb562751
Catherine Armstrong <catherine@sportsdatallc.com>
2014-04-09 14:41:11 -0500

    implemented simple add, subtract, multiply, divide

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,30 @@
-require "calculator/version"
+require 'calculator/version'
 
 module Calculator
-  # Your code goes here...
+    class Simple
+        def initialize(type = :simple)
+            @type = type
+        end
+
+        def add(a, b)
+            a + b
+        end
+
+        def subtract(a, b)
+            a - b
+        end
+
+        def multiply(a, b)
+            a * b
+        end
+
+        def divide(a, b)
+            if b == 0
+                0
+            else
+                a / b
+            end
+        end
+    end
+
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,19 +1,27 @@
 require 'spec_helper'
 
 describe Calculator do
+    before(:each) do
+        @calc = Calculator::Simple.new
+    end
+
     it 'adds two numbers' do
-        pending
+        @calc.add(1,2).should == 3
     end
 
     it 'subtracts two numbers' do
-        pending
+        @calc.subtract(2,1).should == 1
     end
 
     it 'multiples two numbers' do
-        pending
+        @calc.multiply(2,3).should == 6
     end
 
     it 'divides two numbers 'do
-        pending
+        @calc.divide(4,2).should == 2
+    end
+
+    it 'returns 0 if divide by zero' do
+        @calc.divide(4,0).should == 0
     end
 end
